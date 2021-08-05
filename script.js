cee31b62a82de83a9658eb7ae979a807a6af84e5
Isaac Fiedler <fiedlerisaac01@gmail.com>
2021-08-05 07:30:17 -0500

    A small commit, script own file, made functions

---
+++ script.js
@@ -0,0 +1,20 @@
+let number1 = 0;
+let number2 = 0;
+let operation = "nothing";
+const newhtml = "<h1>enter your stuff</h1>"
+
+document.getElementById("calcdisplay").innerHTML = "<h1>asdf</h1>";
+
+function clear() {
+    number1 = 0;
+    number2 = 0;
+    operation = "nothing";
+    newhtml = "<h1>cleared</h1>";
+    // cleared is placeholder
+}
+
+function refresh() {
+    number1 = 0;
+    number2 = 0;
+    // will include new html
+}
